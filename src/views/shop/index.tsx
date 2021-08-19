e6530ff4350e3e99053ba9091e0fd5986f303849
jvictorsouza <victor.souza@lapisco.ifce.edu.br>
2021-08-19 10:53:04 -0300

    init shop page view module

---
+++ src/views/shop/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Styles } from "./styles";
+
+const Shop: React.FC = (...props) => {
+
+  return (
+    <Styles>
+        <span>It's shop time</span>
+    </Styles>
+  );
+};
+
+export default Shop;
