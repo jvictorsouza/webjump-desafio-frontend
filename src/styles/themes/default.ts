518c68217dc9408b2575e427cbbf5a912a62266d
jvictorsouza <victor.souza@lapisco.ifce.edu.br>
2021-08-19 10:49:07 -0300

    add project style files and themes

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+import { DefaultTheme } from "styled-components";
+
+export default {
+  title: "default",
+
+  colors: {
+    zero: "#231f20",
+    one: "#ffffff", 
+    two: "#cc0d1f",
+    three: "#28a3aa",
+    four: "#acacac",
+    five: "#e2dedb",
+    six: "#9a2e2a",
+  },
+} as DefaultTheme;
